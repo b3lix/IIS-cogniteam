0da263349673aa23c438917b1a70d349f17b562d
Michal Slesar <michal.slesar@protonmail.com>
2021-11-17 18:34:01 +0100

    Add CORS

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,6 @@
 # Flask related imports
 from flask import Flask
+from flask_cors import CORS
 from flask_login import LoginManager
 
 # SQLalchemy related imports
@@ -38,6 +39,9 @@ def create_app():
     """
     app = Flask(__name__)
     app.secret_key = SECRET_KEY
+
+    # Setup CORS
+    CORS(app, supports_credentials=True)
 
     # Setup flask login manager
     login_manager = LoginManager()
